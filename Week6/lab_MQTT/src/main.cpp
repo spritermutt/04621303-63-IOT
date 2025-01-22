80cf7dee63ba215ac7fad18ec0300153ff01afca
chusanapak fongmanee <141717296+spritermutt@users.noreply.github.com>
2025-01-22 16:30:32 +0700

    Fix LED

--- Week6/lab_MQTT/src/main.cpp
+++ Week6/lab_MQTT/src/main.cpp
@@ -1,18 +1,91 @@
-#include <Arduino.h>
+#include <ESP8266WiFi.h>
+#include <PubSubClient.h>
 
-// put function declarations here:
-int myFunction(int, int);
+// Update thesewith values suitable for your network.
+#define LED D1
+#define SW D2
+const char* ssid = "TP-Link_A287";
+const char* password = "66845355";
+const char* mqtt_server = "broker.emqx.io";
+WiFiClient espClient;
+PubSubClient client(espClient);
+unsigned long lastMsg = 0;
+#define MSG_BUFFER_SIZE	(50)
+char msg[MSG_BUFFER_SIZE];
+int value = 0;
+void setup_wifi() {
+  delay(10);
+  Serial.println();
+  Serial.print("Connecting to ");
+  Serial.println(ssid);
+  WiFi.mode(WIFI_STA);
+  WiFi.begin(ssid, password);
+  while (WiFi.status() != WL_CONNECTED) {
+    delay(500);
+    Serial.print(".");
+  }
+
+  randomSeed(micros());
+  Serial.println("");
+  Serial.println("WiFi connected");
+  Serial.println("IP address: ");
+  Serial.println(WiFi.localIP());
+}
+
+void callback(char* topic, byte* payload, unsigned int length) {
+  for (int i = 0; i < length; i++) {
+    Serial.print((char)payload[i]);
+  }
+  Serial.println();
+
+/*
+  // Switch on the LED if an 1 was received as first character
+  if ((char)payload[0] == 'on') {
+    digitalWrite(LED, HIGH);
+
+  } else if((char)payload[0] == 'off') {
+    digitalWrite(LED, LOW);  // Turn the LED off by making the voltage HIGH
+  }
+*/
+}
+
+void reconnect() {
+  // Loop until we're reconnected
+  while (!client.connected()) {
+    Serial.print("Attempting MQTT connection...");
+    // Create a random client ID
+    String clientId = "ESP8266Client-";
+    clientId += String(random(0xffff), HEX);
+    // Attempt to connect
+    if (client.connect(clientId.c_str())) {
+      Serial.println("connected");
+      // Once connected, publish an announcement...
+      client.publish("Group1/realpeach", "hello world");
+      // ... and resubscribe
+      client.subscribe("Group1/realpeach");
+    } else {
+      Serial.print("failed, rc=");
+      Serial.print(client.state());
+      Serial.println(" try again in 5 seconds");
+      // Wait 5 seconds before retrying
+      delay(5000);
+    }
+  }
+}
 
 void setup() {
-  // put your setup code here, to run once:
-  int result = myFunction(2, 3);
+  pinMode(LED, OUTPUT);     // Initialize the LED pin as an output
+  Serial.begin(115200);
+  setup_wifi();
+  client.setServer(mqtt_server, 1883);
+  client.setCallback(callback);
 }
 
 void loop() {
-  // put your main code here, to run repeatedly:
-}
 
-// put function definitions here:
-int myFunction(int x, int y) {
-  return x + y;
+  if (!client.connected()) {
+    reconnect();
+  }
+  client.loop();
+
 }
